2c928e8a4bdf0ba9d2dfc5063b968e7741295ebc
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-12-11 12:35:47 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+
+	<!-- Bootstrap Link -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+	<!-- External CSS -->
+	<link rel="stylesheet" type="text/css" href="./index.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+	  <div class="container-fluid">
+	    <!-- Logo -->
+	    <a class="navbar-brand ms-3" href="#landing" id="logo">Matt Lanuza</a>
+	    
+	    <!-- Hamburger Menu -->
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    
+	    <!-- Navbar Links -->
+	    <div class="collapse navbar-collapse p-2" id="navbarNav">
+	      <ul class="navbar-nav ms-auto d-flex gap-3">
+	        <li class="nav-item">
+	          <a class="nav-link active" aria-current="page" href="#landing">Home</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#projects">Projects</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#tools">Tools</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#contact">Contact</a>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+
+	<!-- Bootstrap Script -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
